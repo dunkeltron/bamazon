ddd6ea1e0ce647ef3ff698abccbab7d112c09124
Ian Dunkelberg <idunkelberg@gmail.com>
2018-10-29 20:47:19 -0700

    Remembered to create repo 2 days into the project.

---
+++ bamazonCustomer.js
@@ -0,0 +1,90 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "productsDB"
+});
+
+function printProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log("item_id  |  Name  |  Price  |  Quantity");
+        var arr = [];
+        res.forEach(element => {
+            console.log(element.item_id + " | " + element.product_name + " | " + element.price + "  |  " + element.stock_quantity);
+        });
+        customerInput();
+    });
+}
+
+function customerInput() {
+    inquirer.prompt([{
+            name: "id",
+            message: "What is the item_id of the product you would like to buy?"
+        },
+        {
+            name: "amount",
+            message: "How many items do you want to buy?"
+        }
+    ]).then(function (answer) {
+        connection.query("SELECT * FROM products WHERE item_id = " + answer.id, function (err, res) {
+            if (err) throw err;
+            else if (parseInt(res[0].stock_quantity) < parseInt(answer.amount) || res[0].stock_quantity == 0) {
+                insufficientStock(res[0].product_name, res[0].stock_quantity);
+
+            } else if (parseInt(answer.amount) < 0) {
+                incorrectAmount();
+            } 
+                else {
+                updateStock(parseInt(answer.id), parseInt(answer.amount));
+                printOrder(res[0], parseInt(answer.amount));
+            }
+
+        })
+    })
+}
+
+function incorrectAmount() {
+    console.log("Invalid amount. Cannot buy a negative amount of items.");
+    connection.end();
+}
+
+function insufficientStock(name, stock) {
+    console.log("We don't have enough stock of that item to complete this order.");
+    console.log("We currently have " + stock +" " + name + "(s) in stock.")
+    connection.end();
+}
+
+function updateStock(id, amt) {
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amt + " WHERE item_id = " + id + ";", function (err, res) {
+        if (err) throw err;
+    });
+    connection.end();
+}
+
+function printOrder(res, amount) {
+    console.log("Purchase made! You bought " + amount + " " + res.product_name + "(s) for $" + (res.price * amount) + ".");
+
+}
+printProducts();
+
+/* bonus sql queries for bamazonManager.js 
+    UPDATE products SET stock_quantity = stock_quantity - 1 WHERE item_id = 9;
+
+    SELECT * FROM products WHERE stock_quantity <=5;
+
+    UPDATE products SET stock_quantity = stock_quantity + amt WHERE item_id = id;
+
+    INSERT INTO products (product_name,department_name,price,stock_quantity)
+    VALUES ("itemName", "department", price, amt); */
---
+++ schema.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS productsDB;
+
+CREATE DATABASE productsDB;
+
+USE productsDB;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(45) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price INT(10) NOT NULL,
+    stock_quantity INT(4) NOT NULL,
+    primary key(item_id)
+);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('Socks', 'Clothing', 10, 5);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('HDMI Cable', 'Electronics', 10, 2);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Dinosaur Phone Holder", "Home & Office", 5, 500);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Coffee Beans", "Grocery", 6, 25);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Blender", "Kitchen", 50, 6);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Smoke Alarm", "Home & Office", 23, 60);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Dog Leash", "Pet Supplies", 11, 16);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Oil Filter", "Automotive", 6, 16);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Led Desk Lamp", "Home & Office", 30, 3);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Dinner Fork Set. (12 Pieces)", "Kitchen", 13, 6);
+
